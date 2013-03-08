93564496050133995d30b0d3a726ee19e213174b
Justin Force <justin.force@gmail.com>
2013-03-07 18:54:01 -0800

    Works as advertised

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 inotify_update_tables
 =====================
 
-Watch a directory for newly created files. If a file is created, read an IP
-address from it and insert a -j ACCEPT for that IP into the firewall at position
+Watch a directory for newly created files. If a file is created, use its name as
+an IP address and insert a -j ACCEPT for that IP into the firewall at position
 10.
 
 This is obviously a very useful program for one very specific purpose. :)
---
+++ inotify_update_iptables.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#
+# Usage:
+#   ./inotify_update_iptables.py
+#
+# Watches the ./inotify_update_ipables directory for created files, then adds an
+# exception to the firewall for the IP address specified by the file name.
+
+import subprocess
+import sys
+import pyinotify
+
+class OnCreateHandler(pyinotify.ProcessEvent):
+
+    def process_IN_CREATE(self, event):
+        try:
+            print 'Exempting {0}'.format(event.name)
+            subprocess.call([
+                'echo', # uncomment to debug
+                'iptables',
+                '-I INPUT 10 -s {0} -j ACCEPT'.format(event.name)
+                ])
+        except Exception as e:
+            print e
+
+if __name__ == '__main__':
+    wm = pyinotify.WatchManager()
+    notifier = pyinotify.Notifier(wm, default_proc_fun=OnCreateHandler())
+    wm.add_watch('inotify_update_iptables', pyinotify.ALL_EVENTS)
+    notifier.loop()
