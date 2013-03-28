c709d8e3731d9b69abdb2f6866b706d02bcc567b
Justin Force <justin.force@gmail.com>
2013-03-27 17:46:33 -0700

    Refactor: make python python-i-er

--- inotify_update_iptables.py
+++ inotify_update_iptables.py
@@ -13,16 +13,13 @@ import pyinotify
 class OnCreateHandler(pyinotify.ProcessEvent):
 
     def process_IN_CREATE(self, event):
+        ip_address = event.name
+        command = 'iptables -I INPUT 10 -s {0} -j ACCEPT'.format(ip_address)
         try:
-            print 'Exempting {0}'.format(event.name)
-            subprocess.call([
-                'iptables',
-                '-I', 'INPUT', '10',
-                '-s', event.name,
-                '-j', 'ACCEPT',
-                ])
+            print('Exempting {0}'.format(ip_address))
+            subprocess.call(command.split())
         except Exception as e:
-            print e
+            print(e)
 
 if __name__ == '__main__':
     wm = pyinotify.WatchManager()
